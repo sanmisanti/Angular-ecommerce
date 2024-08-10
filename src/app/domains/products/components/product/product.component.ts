31743f92abf3cb64d3a067412af21f6b4aa02beb
sanmisanti <sanmisanti@gmail.com>
2024-08-09 23:14:33 -0300

    fin clase 30

---
+++ src/app/domains/products/components/product/product.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent {
+  img = 'https://picsum.photos/640/640?r=' + Math.random();
+}
---
+++ src/app/domains/products/pages/list/list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ProductComponent } from './../../components/product/product.component'
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-list',
+  standalone: true,
+  imports: [CommonModule, ProductComponent],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+
+}
